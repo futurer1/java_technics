550eb3e49c15bccc790883f5fc51d603c2fce97c
Mikhail <futurer@inbox.ru>
2023-10-17 23:11:26 +0300

    Create Main.java

---
+++ lambda/Main.java
@@ -0,0 +1,14 @@
+import lambda.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Example ex1 = new Example();
+        Example.example1();
+        Example.example2();
+        Example.example3();
+        Example.example4();
+        Example.example5();
+        Example.example6();
+        Example.example7();
+    }
+}
