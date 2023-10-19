27053b394b35016eee98844575360c256733ac71
Mikhail <futurer@inbox.ru>
2023-10-19 12:18:18 +0300

    working with files

---
+++ file_process/Main.java
@@ -0,0 +1,22 @@
+package readfile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Example.example1();
+
+        // разделитель при указании путей к файлу, использующийся в файловой системе данной ОС
+        String separator = File.separator; 
+      
+        String p = String.format(
+                "java_technics%sfile_process%s",
+                separator,
+                separator
+        );
+      
+        Example.example2(p, "log1.log");
+        Example.example3(p, "log2.log");
+    }
+}
