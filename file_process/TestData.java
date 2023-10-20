909b67ac2017a3ad622084bdd82e210ce2578d9b
Mikhail <futurer@inbox.ru>
2023-10-20 18:45:51 +0300

    modificator transient

--- file_process/TestData.java
+++ file_process/TestData.java
@@ -5,11 +5,19 @@ import java.io.Serializable;
 public class TestData implements Serializable {
     private int id;
     private String name;
+    private transient String password; // Поле, значение которого не будет сериализовываться. Ему будет присвоено значение null
 
     public TestData(int id, String name)
     {
         this.id = id;
         this.name = name;
+    }
+
+    public TestData(int id, String name, String password)
+    {
+        this.id = id;
+        this.name = name;
+        this.password = password;
     }
 
     @Override
