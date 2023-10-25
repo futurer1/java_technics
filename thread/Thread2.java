c6f232be12b0d479c0dc16a0dd7a83e9c1e5bb5b
Mikhail <futurer@inbox.ru>
2023-10-25 15:14:31 +0300

    Create Thread2.java

---
+++ thread/Thread2.java
@@ -0,0 +1,45 @@
+package thread;
+
+import java.util.Scanner;
+
+public class Thread2 {
+    public static void main(String[] args) {
+        example1();
+    }
+
+    /**
+     * Ввод в консоль останавливает поток MyThread2
+     */
+    public static void example1() {
+        MyThread2 myThread2 = new MyThread2();
+        myThread2.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine(); // ждем ввод в консоль
+        myThread2.shutdown();
+    }
+}
+
+class MyThread2 extends Thread {
+    /**
+     * условие работы потока. Значение не кешируется.
+     */
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("MyThread2 is running...");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void shutdown() {
+        System.out.println("MyThread2 is shutting down...");
+        running = false;
+    }
+}
