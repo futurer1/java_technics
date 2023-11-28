cc394e10c8c7fbdae7d67142ad467b2e8ea7a32f
Mikhail <futurer@inbox.ru>
2023-11-28 18:43:21 +0300

    env

--- utility.java
+++ utility.java
@@ -11,3 +11,9 @@ if ("Sometext".equals(param)) {}
 // узнать доступное кол-во процессоров
 // можно сделать многопоточку, которая нагрузит все ресурсы равномерно
 int a = Runtime.getRuntime().availableProcessors();
+
+// вывести переменные окружения построчно
+Map<String, String> envs = System.getenv();
+for (Map.Entry<String, String> env: envs.entrySet()) {
+    System.out.println(env.getKey() + " - " + env.getValue());
+}
