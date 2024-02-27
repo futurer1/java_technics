a326989a672caeb61708813541dfcdeffabe2ea8
Mikhail <futurer@inbox.ru>
2024-02-27 22:52:26 +0300

    Update Example.java

--- record/Example.java
+++ record/Example.java
@@ -1,4 +1,13 @@
+// компилятор преобразует record в final class 
+// автоматически генерируются методы 
+// toString(), hashCode() и equals()
+// и параметризованный конструктор 
+// геттеры не имеют приставки get, называются как параметры 
+
 public record Example<T, U>(T first, U second) {}
 
 // пример использования 
 Example<String, Integer> a = new Example<>( "Hi!" , 123);
+
+String name = a.first();
+int age = a.second();
