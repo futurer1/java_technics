7adc4edf035b098eedbaaabec3b9995d322e2215
Mikhail <futurer@inbox.ru>
2024-03-05 19:30:56 +0300

    Update KafkaConfig.java

--- kafka/config/KafkaConfig.java
+++ kafka/config/KafkaConfig.java
@@ -9,7 +9,7 @@ public class KafkaConfiguration {
         return TopicBuilder.name("product-created-events-topic")
           .partitions(3) // 3 партиции
           .replicas(3) // 3 реплики
-          .configs(Map.of("min.insync.replicas", "2")) // минимальное кол-во 2 сервера в синхронизации с лидером (параметр Isr при вызове --describe)
+          .configs(Map.of("min.insync.replicas", "2")) // минимальное кол-во 2 сервера в синхронизации с лидером (параметр Isr при вызове команды --describe из консоли)
           // рекомендуется на 1 меньше, чем всего реплик
           .build();
     }
